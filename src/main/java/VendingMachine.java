9012c8576f594a6ec0ae364acced3fa212b7ab5d
micaela berry <msberry9@gmail.com>
2017-08-25 09:29:58 -0400

    tests

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,8 +1,36 @@
 
 public class VendingMachine {
 
+	private double nickle = 0.05;
+	private double dime = 0.10;
+	private double quarter = 0.25;
+	private Double totalMoney = 0.0;
+
 	public boolean insertNickle() {
+		totalMoney += nickle;
 		return true;
 	}
 
+	public boolean insertDime() {
+		totalMoney += dime;
+		return true;
+	}
+
+	public boolean insertQuarter() {
+		totalMoney += quarter;
+		return true;
+	}
+
+	public boolean insertPenny() {
+		return false;
+	}
+
+	public String getDisplay() {
+		if (totalMoney == 0.0) {
+			return "INSERT COIN";
+		}		
+		return totalMoney.toString();
+	}
+
+	
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,13 +1,75 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
+	
+	private VendingMachine vendingMachine;
+	
+	@Before
+	public void setUp(){
+		vendingMachine = new VendingMachine();
+	}
 
 	@Test
 	public void whenNickleIsPassedInVendingMachineThenReturnTrue(){
-		VendingMachine vendingMachine = new VendingMachine();
-		assertEquals(true,vendingMachine.insertNickle());
+		assertTrue(vendingMachine.insertNickle());
 	}
+	
+	@Test
+	public void whenDimeIsPassedInVendingMachineThenReturnTrue(){
+		assertTrue(vendingMachine.insertDime());
+	}
+	
+	@Test
+	public void whenQuarterIsPassedInVendingMachineThenReturnTrue(){
+		assertTrue(vendingMachine.insertQuarter());
+	}
+	
+	@Test
+	public void whenPennyIsPassedInVendingMachineThenReturnFalse(){
+		assertFalse(vendingMachine.insertPenny());
+	}
+	
+	@Test
+	public void whenGetDispalyIsCalledThenInsertCoinIsDisplayed(){ 
+		assertTrue("actual "+ vendingMachine.getDisplay(), vendingMachine.getDisplay().equals("INSERT COIN"));
+	}
+	
+	@Test
+	public void insertNickleIntoTheVendingMachineDisplaySaysFiveCents(){
+		vendingMachine.insertNickle();
+		assertTrue(vendingMachine.getDisplay().equals("0.05"));
+	}
+	
+	@Test
+	public void insertDimeIntoTheVendingMachineDisplaySaysTenCents(){
+		vendingMachine.insertDime();
+		assertTrue("actual "+ vendingMachine.getDisplay(), vendingMachine.getDisplay().equals("0.1"));
+	}
+	
+	@Test
+	public void insertQuarterIntoTheVendingMachineDisplaySaysTwentyFiveCents(){
+		vendingMachine.insertQuarter();
+		assertTrue(vendingMachine.getDisplay().equals("0.25"));
+	}
+	
+	@Test
+	public void whenMultipleCoinsOfTheSameValueAreInserted(){
+		vendingMachine.insertQuarter();
+		vendingMachine.insertQuarter();
+		assertTrue("actual "+ vendingMachine.getDisplay(), vendingMachine.getDisplay().equals("0.5"));		
+	}
+	
+	@Test
+	public void whenMultipleCoinsOfDifferentValuesAreInserted(){
+		vendingMachine.insertQuarter();
+		vendingMachine.insertDime();
+		assertTrue("actual "+ vendingMachine.getDisplay(), vendingMachine.getDisplay().equals("0.35"));
+	}
+	
+	
+	
 	
 }
